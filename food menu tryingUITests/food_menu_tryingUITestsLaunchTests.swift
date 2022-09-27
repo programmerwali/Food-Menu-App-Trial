79c4fb28d017e878993891db929a48c6e5b52c88
programmerwali <91944091+programmerwali@users.noreply.github.com>
2022-09-27 06:16:17 +0500

    Initial Commit

---
+++ food menu tryingUITests/food_menu_tryingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  food_menu_tryingUITestsLaunchTests.swift
+//  food menu tryingUITests
+//
+//  Created by Wali Faisal on 27/09/2022.
+//
+
+import XCTest
+
+class food_menu_tryingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
